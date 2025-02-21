472f723e515373f963fef79af34b4a7017cce60b
ratatatcode <pogisijamesmagnaye@gmail.com>
2025-02-21 22:52:16 +0800

    Day 1: Add routes and controllers folder; Refactor server.js to work with routes

--- server.js
+++ server.js
@@ -1,16 +1,18 @@
 const express = require("express");
 const app = express();
 const path = require("path");
-const ejs = require("ejs");
+
+// routes
+const auth = require("./routes/user-auth");
 
 // consider using reverse proxy: https://expressjs.com/en/advanced/best-practice-performance.html#use-a-reverse-proxy
 // https://expressjs.com/en/starter/static-files.html
 app.use('/static', express.static(path.join(__dirname, 'public')));
 app.set('view engine', 'ejs');
 
-app.get('/', (req, res) => {
-    res.render("landing-page.ejs");
-});
+
+// direct to login
+app.use('/', auth);
 
 // npx nodemon server.js
 const PORT = 3000;
